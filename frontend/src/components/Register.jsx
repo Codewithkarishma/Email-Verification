0cd655fa09ad329f4714831fa91ddb0abcdcc111
Karishma <karishmakhatri390@gmail.com>
2024-10-26 10:59:58 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/register', {email, password});
+      const response = await axios.post('https://email-verification-backend.onrender.com/register', {email, password});
       toast.success(response.data.message);
       setEmail('');
       setPassword('');
